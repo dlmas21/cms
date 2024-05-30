67a3ac2ee337248dd5fc2e5b7cf43904428d47d1
dlmas21 <dimitriyroman@yandex.ru>
2024-05-31 01:18:36 +0300

    1

--- src/api/proekty/controllers/proekty.ts
+++
@@ -1,7 +0,0 @@
-/**
- * proekty controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::proekty.proekty');
--- src/api/proekty/services/proekty.ts
+++
@@ -1,7 +0,0 @@
-/**
- * proekty service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::proekty.proekty');
